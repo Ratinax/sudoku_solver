fd611d6e78a162a6919eaa6342ea7158a6647a9c
tbernot <tbernot@hackuity.io>
2024-03-12 14:06:31 +0100

    better main

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
-for i in range(2):
-	print(i)
+import sys
+from random import randint
+def hide_cursor():
+	sys.stdout.write('\033[?25l')
+	sys.stdout.flush()
+def show_cursor():
+	sys.stdout.write('\033[?25h')
+	sys.stdout.flush()
+hide_cursor()
+for k in range(10000):
+	for i in range(9):
+		for j in range(9):
+			sys.stdout.write(str((k) % 9))
+		sys.stdout.write("\n")
+
+	sys.stdout.write("\033[F\033[F\033[F\033[F\033[F\033[F\033[F\033[F\033[F")
+for i in range(9):
+	sys.stdout.write("\n")
+
+show_cursor()
